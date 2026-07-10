81eca10c16c746dec545d22a1646f57053fb8ad1
mingodando <thebestballer182@gmail.com>
2026-07-10 14:49:27 +0700

    created the edit profile page

--- courtbooking/courts/urls.py
+++ courtbooking/courts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'courts'
+
 urlpatterns = [
     path("", views.court_list, name="court-list"),
+    path("profile/edit/", views.edit_profile, name="edit-profile"),
 ]
--- courtbooking/courts/views.py
+++ courtbooking/courts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Court
+from django.contrib.auth.decorators import login_required
+from .forms import UserProfileForm
+from django.shortcuts import render, redirect
+
+
 
 def court_list(request):
     courts = Court.objects.filter(is_active=True)
@@ -7,3 +12,17 @@ def court_list(request):
         "courts": courts,
         "user": request.user,
     })
+
+@login_required #Required login to view the page
+def edit_profile(request):
+    profile = request.user.userprofile  #Gets the profile
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("courts:court-list")
+    else:
+        form = UserProfileForm(instance=profile)
+
+    return render(request, "courts/edit_profile.html", {"form": form})
